9e64300a2b3983de1717cf8887e3ce6bb1e5c917
stephanbazbaz <stephan@pwm.co.il>
2022-12-08 22:13:29 +0200

    init

---
+++ pages/GetPokes/[id].js
@@ -0,0 +1,21 @@
+import styles from '../../styles/style.module.scss'
+
+export default function GetPokes(props) {
+  const { character } = props
+  return (
+    <div className={styles.container}>
+      <h1>poke</h1>
+      <div>{character.growth_time}</div>
+      <div>{character.name}</div>
+      <div>{character.item.url}</div>
+      <div className={styles.row}></div>
+    </div>
+  )
+}
+
+export const getServerSideProps = async (context) => {
+  const { id } = context.params
+  const character = await fetch(`https://pokeapi.co/api/v2/berry/${id}`)
+    .then(res => res.json());
+  return { props: { character } };
+};
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import puppeteer from 'puppeteer';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  await printPDF()
+
   res.status(200).json({ name: 'John Doe' })
+}
+async function printPDF() {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000/GetPokes/1', { waitUntil: 'networkidle0' });
+  await page.emulateMediaType('screen');
+  const pdf = await page.pdf({
+    format: 'A4',
+    path: '/Users/user/Desktop/result.pdf',
+    printBackground: true,
+  });
+
+  await browser.close();
+  return pdf
 }
